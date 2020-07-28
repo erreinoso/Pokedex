2a08f20a4002cf273723a25dcc1fd9b97c0701db
erreinoso <eramirezreinoso@gmail.com>
2020-07-28 10:27:08 +0200

    run docs

---
+++ docs/precache-manifest.06201a964201ecae4aa216e5bd9e214a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c4bbe7eb008b969e09d3035f6fc5f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4b635f830ad7ad4eaf0",
+    "url": "./static/css/main.20a57613.chunk.css"
+  },
+  {
+    "revision": "98efcd7c9b0a10c112f0",
+    "url": "./static/js/2.d760aea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d760aea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b635f830ad7ad4eaf0",
+    "url": "./static/js/main.f758873c.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  },
+  {
+    "revision": "db580bcd611f92809c2d3f9eddf2f668",
+    "url": "./static/media/grass.db580bcd.jpg"
+  }
+]);
